3a705827238f83d8445184c84e209be163b93722
Mikhail Bondar <m.bondar@yahoo.com>
2012-07-10 18:09:40 +0300

    Fix for unknown wasabi error

---
+++ lib/travelport/wasabi/parser.rb
@@ -0,0 +1,6 @@
+Wasabi::Parser.class_eval do
+  def input_for(operation)
+    message_ns_id, message_type = nil                                                                                                                       
+    [message_ns_id, message_type]
+  end
+end
